3d1687480c58f5c12a4e5ad78a88e399a20d50d2
Edoardo Giussani <EGiussani@izsvenezie.it>
2025-04-04 14:19:59 +0200

    feat: changed cli to use new logic

--- src/flumut/cli.py
+++ src/flumut/cli.py
@@ -1,52 +1,77 @@
 import click
 from click import File
 
-import flumut
-from flumut import __version__, __author__, __contact__
+from flumut import __author__, __contact__, __version__
+from flumut.analysis import analyze
+from flumut.db_utility.db_connection import DBConnection
+from flumut.db_utility.db_update import update
+from flumut.output import set_output_file
+from flumut.sequence_utility.fasta_handler import set_header_pattern
 
 
-def update(ctx, param, value):
+def update_db(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
-    old_version = flumut.versions()['FluMutDB']
-    flumut.update()
-    new_version = flumut.versions()['FluMutDB']
+    old_version = DBConnection().version_string
+    update()
+    new_version = DBConnection().version_string
     if old_version == new_version:
-        print(f'Already using latest FluMutDB version ({new_version})')
+        print(f'Already using latest flumutdb version ({new_version})')
     else:
-        print(f'Updated FluMutDB to version {new_version}')
+        print(f'Updated flumutdb to version {new_version}')
     ctx.exit()
 
 
-def versions(ctx, param, value):
+def print_all_versions(ctx, param, value):
     if not value or ctx.resilient_parsing:
         return
-    for package, version in flumut.versions().items():
-        print(f'{package}: {version}')
+    print(f'flumut: {__version__}')
+    major, minor, date = DBConnection().version
+    print(f'flumutdb: {DBConnection().version_string}')
     ctx.exit()
+
+
+def set_name_regex(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    set_header_pattern(value)
+
+
+def set_output(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    set_output_file(param.name, value)
+
+
+def set_dbfile(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    DBConnection().db_file = value
 
 
 @click.command()
+# Help and versions
 @click.help_option('-h', '--help')
 @click.version_option(__version__, '-v', '--version', message=f'%(prog)s, version %(version)s, by {__author__} ({__contact__})')
-@click.option('-V', '--all-versions', is_flag=True, callback=versions, expose_value=False, is_eager=True, help='Prints all versions and exit.')
-@click.option('--update', is_flag=True, callback=update, expose_value=False, is_eager=True, help='Update the database to the latest version and exit.')
-@click.option('-n', '--name-regex', type=str, default=r'(?P<sample>.+)_(?P<segment>.+)', show_default=True, help='Set regular expression to parse sequence name.')
+# Database selection, must be eager since it must be parsed before update and all-versions
+@click.option('-D', '--db-file', type=str, callback=set_dbfile, expose_value=False, is_eager=True, help='Set source database.')
+# Options that exits from the workflow
+@click.option('-V', '--all-versions', is_flag=True, callback=print_all_versions, expose_value=False, help='Prints all versions and exit.')
+@click.option('--update', is_flag=True, callback=update_db, expose_value=False, help='Update the database to the latest version and exit.')
+# Advanced options
 @click.option('--skip-unmatch-names', is_flag=True, default=False, help='Skip sequences with name that does not match the regular expression pattern.')
-@click.option('--skip-unknown-segments', is_flag=True, default=False, help='Skip sequences with segment not present in the database.')
 @click.option('-r', '--relaxed', is_flag=True, help='Report markers of which at least one mutation is found.')
-@click.option('-D', '--db-file', type=str, default=None, help='Set source database.')
-@click.option('-m', '--markers-output', type=File('w', 'utf-8'), default=None, help='TSV markers output file.')
-@click.option('-M', '--mutations-output', type=File('w', 'utf-8'), default=None, help='TSV mutations output file.')
-@click.option('-l', '--literature-output', type=File('w', 'utf-8'), default=None, help='TSV literature output file.')
-@click.option('-x', '--excel-output', type=str, default=None, help='Excel complete report.')
-@click.option('--debug', is_flag=True, hidden=True, help='Output errors with traceback')
-@click.option('--verbose', is_flag=True, hidden=True)
-@click.argument('fasta-file', type=File('r'))
-def cli(name_regex: str, fasta_file: File, db_file: str,
-        markers_output: File, mutations_output: File, literature_output: File, excel_output: str,
-        relaxed: bool, skip_unmatch_names: bool, skip_unknown_segments: bool, debug: bool, verbose: bool) -> None:
-    flumut.analyze(**locals())
+@click.option('-n', '--name-regex', type=str, callback=set_name_regex, expose_value=False, default=r'(?P<sample>.+)_(?P<segment>.+)', show_default=True,
+              help='Set regular expression to parse sequence name.')
+# Output files
+@click.option('-m', '--markers-output', callback=set_output, expose_value=False, type=File('w', 'utf-8'), default=None, help='TSV markers output file.')
+@click.option('-M', '--mutations-output', callback=set_output, expose_value=False, type=File('w', 'utf-8'), default=None, help='TSV mutations output file.')
+@click.option('-l', '--literature-output', callback=set_output, expose_value=False, type=File('w', 'utf-8'), default=None, help='TSV literature output file.')
+@click.option('-x', '--excel-output', callback=set_output, expose_value=False, type=str, default=None, help='Excel complete report.')
+# Input files
+@click.argument('fasta-files', type=File('r'), nargs=-1)
+def cli(fasta_files: File, relaxed: bool, skip_unmatch_names: bool) -> None:
+    analyze(fasta_files, relaxed, skip_unmatch_names)
 
 
 if __name__ == '__main__':
