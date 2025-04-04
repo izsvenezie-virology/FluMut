00262551961145839f7a40ffe24cc2c9e16f7732
Edoardo Giussani <EGiussani@izsvenezie.it>
2025-04-04 14:16:54 +0200

    feat: improved database update logic

---
+++ src/flumut/db_utility/db_update.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+from urllib.request import urlretrieve
+from flumut.db_utility.db_connection import DBConnection
+
+
+def update() -> None:
+    db = DBConnection()
+    if db.is_pip_package:
+        pip_update()
+    else:
+        file_update()
+
+
+def pip_update() -> None:
+    '''Update flumutdb Python package using pip.'''
+    output = subprocess.check_output([sys.executable, '-m', 'pip', 'install', '-U', 'flumutdb'], stderr=subprocess.PIPE)
+    pass
+
+
+def file_update() -> None:
+    '''Download and replace the database file, the Python package is not updated.'''
+    url = 'https://github.com/izsvenezie-virology/FluMutDB/releases/latest/download/flumut_db.sqlite'
+    db_path = DBConnection().db_file
+    _ = urlretrieve(url, db_path)
