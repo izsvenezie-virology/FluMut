260f51bc58221bd82a6cd57a0034faa375aa7d77
giussanie <egiussani@izsvenezie.it>
2026-04-28 18:55:02 +0200

    feat: added TRACE level to logger

--- src/flumut/logger.py
+++ src/flumut/logger.py
@@ -1,14 +1,32 @@
 import logging
 import sys
+from typing import Final
+
+TRACE: Final[int] = logging.DEBUG + 5
+logging.addLevelName(TRACE, 'TRACE')
+
+_logging_levels = {
+    'dbg': logging.DEBUG,
+    'trc': TRACE,
+    'inf': logging.INFO,
+    'wrn': logging.WARNING,
+    'err': logging.ERROR,
+}
 
 
 def initialize_logging(logging_level: str) -> None:
-    """
-    Initialize the logger.
+    logging.basicConfig(
+        level=_logging_levels[logging_level],
+        format='[%(levelname)s] %(message)s',
+        stream=sys.stderr,
+        force=True,
+    )
 
-    :param `str` logging_level: The verbosity level of the logger.
-    """
-    logging.basicConfig(level=_logging_levels[logging_level], format='[%(levelname)s] %(message)s', stream=sys.stderr, force=True)
+
+class Logger(logging.Logger):
+    def trace(self, msg: object, *args: object) -> None:
+        if self.isEnabledFor(TRACE):
+            self._log(TRACE, msg, args)
 
 
-_logging_levels = {'dbg': logging.DEBUG, 'inf': logging.INFO, 'wrn': logging.WARN, 'err': logging.ERROR}
+logging.setLoggerClass(Logger)
