27ff598d20666974f9cad5e1d9d16e03e6f4ac2c
giussanie <egiussani@izsvenezie.it>
2026-05-08 15:14:36 +0200

    feat: removed allow unmatching names
    Now if any unmatching name is found a warning will be logged and the entire header is used as sample.

--- src/flumut/sequence_utility/parser.py
+++ src/flumut/sequence_utility/parser.py
@@ -1,10 +1,8 @@
 import logging
 import re
 
-from flumut.exceptions import UnmatchingHeaderException
 
-
-def parse_header(header: str, pattern: re.Pattern, allow_unmatching_headers: bool = False) -> tuple[str, str | None]:
+def parse_header(header: str, pattern: re.Pattern) -> tuple[str, str | None]:
     """
     Parse the header with the header pattern.
     It searches for two groups in order to retrieve sample and segment from the header.
@@ -19,8 +17,6 @@ def parse_header(header: str, pattern: re.Pattern, allow_unmatching_headers: boo
     match = pattern.match(header)
 
     if not match:
-        if not allow_unmatching_headers:
-            raise UnmatchingHeaderException(header, pattern.pattern) from None
         logging.info(f'Cannot extract sample ID from "{header}". The whole header is used as sample ID.')
         return header, None
 
